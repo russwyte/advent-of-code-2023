9e26ba0a751c25fc7aed162d2c3eb5ebb7989318
Russell White <356303+russwyte@users.noreply.github.com>
2023-12-06 11:39:33 -0600

    use range for both parts

--- src/test/scala/aoc2023/Day6.scala
+++ src/test/scala/aoc2023/Day6.scala
@@ -4,24 +4,19 @@ import scala.math.*
 
 class Day6 extends AocTest:
   import Day6.*
-  def winningHoldsMap(race: Race) =
-    val minWinningHold = race.distance
-    val distances      = (1 to race.time.toInt).map(n => n -> n * (race.time - n))
-    distances.filter((h, d) => d > race.distance).toMap
-  def margine(races: List[Race]) = races.map(winningHoldsMap).map(_.size).reduce(_ * _)
 
-  def countWaysToWin(race: Race): Long =
-    val a     = -1
-    val b     = race.time
-    val c     = -race.distance
-    val D     = sqrt(b * b - 4 * a * c)
-    val high  = ceil((-b - D) / (2 * a)).toLong
-    val low   = floor((-b + D) / (2 * a)).toLong + 1
-    val range = low until high
-    range.size
-  end countWaysToWin
+  def margine(races: List[Race]) = races.map(winningHoldRange).map(_.size).reduce(_ * _)
 
-  def winningHoldRange(race: Race): Range = ???
+  def countWaysToWin(race: Race): Long = winningHoldRange(race).size
+
+  def winningHoldRange(race: Race) =
+    val a    = -1
+    val b    = race.time
+    val c    = -race.distance
+    val D    = sqrt(b * b - 4 * a * c)
+    val high = ceil((-b - D) / (2 * a)).toLong
+    val low  = floor((-b + D) / (2 * a)).toLong + 1
+    low until high
 
   import scala.math.*
 
@@ -34,16 +29,16 @@ class Day6 extends AocTest:
     assertEquals(margine(exampleRaces), 288)
     assertEquals(margine(inputRaces), 4811940)
   test("part2"):
-    val example2 =
+    val examplePart2 =
       """Time:      71530
       |Distance:  940200""".stripMargin
-    val exampleRaces2 = racesSyntax.parseString(example2).toOption.get
-    val dStr          = inputRaces.map(_.distance.toString()).mkString
-    val tStr          = inputRaces.map(_.time.toString()).mkString
-    val bigRacee      = Race(tStr.toLong, dStr.toLong)
-    val exampleRace   = countWaysToWin(exampleRaces2(0))
-    val bigWays       = countWaysToWin(bigRacee)
-    assertEquals(exampleRace, 71503L)
+    val exampleRace = racesSyntax.parseString(examplePart2).toOption.get.head
+    val dStr        = inputRaces.map(_.distance.toString()).mkString
+    val tStr        = inputRaces.map(_.time.toString()).mkString
+    val inputRace   = Race(tStr.toLong, dStr.toLong)
+    val exampleWays = countWaysToWin(exampleRace)
+    val bigWays     = countWaysToWin(inputRace)
+    assertEquals(exampleWays, 71503L)
     assertEquals(bigWays, 30077773L)
 end Day6
 object Day6:
