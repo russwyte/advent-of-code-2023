36b8815d4ee64f641a53ad50faa8b0886569be7d
Russell White <356303+russwyte@users.noreply.github.com>
2023-12-11 17:33:12 -0600

    cleanup

--- src/test/scala/aoc2023/Day11.scala
+++ src/test/scala/aoc2023/Day11.scala
@@ -3,15 +3,15 @@ package aoc2023
 import aoc.*
 
 class Day11 extends AocTest:
-  val in              = input.toVector
-  val starMap         = Area(in)
-  val galaxies        = starMap.pointsIterator.filter(in(_) == '#').toVector
-  val occupiedRows    = galaxies.map(_.y).toSet
-  val occupiedColumns = galaxies.map(_.x).toSet
+  val in: Vector[String]        = input.toVector
+  val starMap: Area             = Area(in)
+  val galaxies: Vector[Point]   = starMap.pointsIterator.filter(in(_) == '#').toVector
+  val occupiedRows: Set[Int]    = galaxies.map(_.y).toSet
+  val occupiedColumns: Set[Int] = galaxies.map(_.x).toSet
   def measure(a: Point, b: Point, growth: Long): Long =
-    val bounds       = Area.bounding(a, b)
-    val emptyColumns = bounds.xRange.count(!occupiedColumns(_))
-    val emptyRows    = bounds.yRange.count(!occupiedRows(_))
+    val bounds: Area      = Area.bounding(a, b)
+    val emptyColumns: Int = bounds.xRange.count(!occupiedColumns(_))
+    val emptyRows: Int    = bounds.yRange.count(!occupiedRows(_))
     a.manhattanDistance(b) + (emptyColumns + emptyRows) * (growth - 1)
   def trips(growth: Long) = galaxies.combinations(2).collect { case Vector(a, b) => measure(a, b, growth) }
   test("part1"):
