8f062251086e804256dfa231aa44a4d2c77fb196
Russell White <356303+russwyte@users.noreply.github.com>
2023-12-18 17:05:50 -0600

    optimize Dijkstra

--- src/main/scala/aoc/graph/Dijktra.scala
+++ src/main/scala/aoc/graph/Dijktra.scala
@@ -7,62 +7,60 @@ object Dijkstra:
   def traverse[A](graphTraversal: GraphTraversal[A]): Distances[A] =
     val visitedDistance: mutable.Map[A, Int]     = mutable.Map.empty
     val toVisit: mutable.PriorityQueue[(Int, A)] = mutable.PriorityQueue.empty(Ordering.by(-_._1))
+    val visited: mutable.HashSet[A]              = mutable.HashSet.empty
 
     def enqueue(node: A, dist: Int): Unit =
-      toVisit.enqueue((dist, node))
+      if !visited(node) then
+        toVisit.enqueue((dist, node))
+        visited += node
 
     enqueue(graphTraversal.startNode, 0)
 
     while toVisit.nonEmpty do
       val (dist, node) = toVisit.dequeue()
-      if !visitedDistance.contains(node) then
-        visitedDistance(node) = dist
+      visitedDistance(node) = dist
 
-        def goNeighbor(newNode: A, distDelta: Int): Unit =
-          if !visitedDistance.contains(newNode) then // avoids some unnecessary queue duplication but not all
-            val newDist = dist + distDelta
-            enqueue(newNode, newDist)
+      def goNeighbor(newNode: A, distDelta: Int): Unit =
+        val newDist = dist + distDelta
+        enqueue(newNode, newDist)
 
-        graphTraversal.neighbors(node).iterator.foreach((goNeighbor _).tupled) // eta expansion postfix operator _
+      graphTraversal.neighbors(node).iterator.foreach((goNeighbor _).tupled)
     end while
 
     new Distances[A]:
       override def distances: collection.Map[A, Int] = visitedDistance
   end traverse
 
-  // copied from AStar
   def search[A](graphSearch: GraphSearch[A]): Distances[A] with Target[A] =
     val visitedDistance: mutable.Map[A, Int]     = mutable.Map.empty
     val toVisit: mutable.PriorityQueue[(Int, A)] = mutable.PriorityQueue.empty(Ordering.by(-_._1))
+    val visited: mutable.HashSet[A]              = mutable.HashSet.empty
 
     def enqueue(node: A, dist: Int): Unit =
-      toVisit.enqueue((dist, node))
+      if !visited(node) then
+        toVisit.enqueue((dist, node))
+        visited += node
 
     enqueue(graphSearch.startNode, 0)
 
     while toVisit.nonEmpty do
       val (dist, node) = toVisit.dequeue()
-      if !visitedDistance.contains(node) then
-        visitedDistance(node) = dist
+      visitedDistance(node) = dist
 
-        if graphSearch.isTargetNode(node, dist) then
-          return new Distances[A] with Target[A]:
-            override def distances: collection.Map[A, Int] = visitedDistance
+      if graphSearch.isTargetNode(node, dist) then
+        return new Distances[A] with Target[A]:
+          override def distances: collection.Map[A, Int] = visitedDistance
+          override def target: Option[(A, Int)]          = Some(node -> dist)
 
-            override def target: Option[(A, Int)] = Some(node -> dist)
+      def goNeighbor(newNode: A, distDelta: Int): Unit =
+        val newDist = dist + distDelta
+        enqueue(newNode, newDist)
 
-        def goNeighbor(newNode: A, distDelta: Int): Unit =
-          if !visitedDistance.contains(newNode) then // avoids some unnecessary queue duplication but not all
-            val newDist = dist + distDelta
-            enqueue(newNode, newDist)
-
-        graphSearch.neighbors(node).iterator.foreach((goNeighbor _).tupled) // eta expansion postfix operator _
-      end if
+      graphSearch.neighbors(node).iterator.foreach((goNeighbor _).tupled)
     end while
 
     new Distances[A] with Target[A]:
       override def distances: collection.Map[A, Int] = visitedDistance
-
-      override def target: Option[(A, Int)] = None
+      override def target: Option[(A, Int)]          = None
   end search
 end Dijkstra
