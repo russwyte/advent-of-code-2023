0cf71699e9c5b03fd9d6857efdd0475141ea6174
Russell White <356303+russwyte@users.noreply.github.com>
2024-01-04 16:16:11 -0600

    Day 25

--- src/main/scala/aoc/graph/BFS.scala
+++ src/main/scala/aoc/graph/BFS.scala
@@ -65,6 +65,47 @@ object BFS:
       override def target: Option[(A, Int)] = None
   end search
 
+  def searchPaths[A](graphSearch: GraphSearch[A] with UnitNeighbors[A]): Distances[A] with Paths[A] with Target[A] =
+    val visitedDistance: mutable.Map[A, Int]        = mutable.Map.empty
+    val prevNode: mutable.Map[A, A]                 = mutable.Map.empty
+    val toVisit: mutable.Queue[(Int, Option[A], A)] = mutable.Queue.empty
+
+    def enqueue(oldNode: Option[A], node: A, dist: Int): Unit =
+      toVisit.enqueue((dist, oldNode, node))
+
+    enqueue(None, graphSearch.startNode, 0)
+
+    while toVisit.nonEmpty do
+      val (dist, oldNode, node) = toVisit.dequeue()
+      if !visitedDistance.contains(node) then
+        visitedDistance(node) = dist
+        for oldNode <- oldNode do prevNode(node) = oldNode
+
+        if graphSearch.isTargetNode(node, dist) then
+          return new Distances[A] with Paths[A] with Target[A]:
+            override def distances: collection.Map[A, Int] = visitedDistance
+
+            override def prevNodes: collection.Map[A, A] = prevNode
+
+            override def target: Option[(A, Int)] = Some(node -> dist)
+
+        def goNeighbor(newNode: A): Unit =
+          if !visitedDistance.contains(newNode) then // avoids some unnecessary queue duplication but not all
+            val newDist = dist + 1
+            enqueue(Some(node), newNode, newDist)
+
+        graphSearch.unitNeighbors(node).iterator.foreach(goNeighbor)
+      end if
+    end while
+
+    new Distances[A] with Paths[A] with Target[A]:
+      override def distances: collection.Map[A, Int] = visitedDistance
+
+      override def prevNodes: collection.Map[A, A] = prevNode
+
+      override def target: Option[(A, Int)] = None
+  end searchPaths
+
   def components[A](graphComponents: GraphComponents[A]): collection.Set[collection.Set[A]] =
 
     def bfs(start: A): collection.Set[A] =
--- src/main/scala/aoc/graph/Graph.scala
+++ src/main/scala/aoc/graph/Graph.scala
@@ -1,4 +1,7 @@
 package aoc.graph
+import scala.collection.mutable
+import scala.util.boundary
+import scala.util.boundary.break
 
 trait GraphTraversal[A]:
   val startNode: A
@@ -37,3 +40,58 @@ trait Target[A]:
 trait GraphComponents[A]:
   def nodes: IterableOnce[A]
   def unitNeighbors(node: A): IterableOnce[A]
+
+trait Paths[A]:
+  extension (l: scala.collection.immutable.LazyList.type)
+    def unfold0[A](a: A)(f: A => Option[A]): LazyList[A] =
+      LazyList.unfold(a)(a => f(a).map(a => (a, a)))
+  def prevNodes: collection.Map[A, A]
+
+  def paths: collection.Map[A, Seq[A]] =
+    prevNodes.map((node, _) => node -> (node +: LazyList.unfold0(node)(prevNodes.get)).reverse)
+
+case class Edge[A](a: A, b: A)
+def edmondsKarp[A](
+    edges: Set[Edge[A]],
+    startNode0: A,
+    targetNode0: A,
+): (Int, collection.Map[A, collection.Map[A, Int]]) =
+
+  val residual: mutable.Map[A, mutable.Map[A, Int]] = mutable.Map.empty
+
+  for Edge(u, v) <- edges do
+    if !residual.contains(u) then residual(u) = mutable.Map.empty
+    if !residual.contains(v) then residual(v) = mutable.Map.empty
+    residual(u)(v) = 1
+    residual(v)(u) = 1
+
+  var maxFlow = 0
+
+  boundary {
+    while true do
+      val graphSearch = new GraphSearch[A] with UnitNeighbors[A] with TargetNode[A]:
+        override val startNode: A = startNode0
+
+        override def unitNeighbors(node: A): IterableOnce[A] =
+          for
+            (toNode, r) <- residual(node)
+            if r > 0
+          yield toNode
+
+        override val targetNode: A = targetNode0
+
+      val searchResult = BFS.searchPaths(graphSearch)
+      if searchResult.target.isEmpty then break()
+      else
+        val targetPath = searchResult.paths(targetNode0)
+        val pathEdges  = targetPath lazyZip targetPath.tail
+        val pathFlow   = pathEdges.map(residual(_)(_)).min
+        for (u, v) <- pathEdges do
+          residual(u)(v) -= pathFlow
+          residual(v)(u) += pathFlow
+        maxFlow += pathFlow
+      end if
+  }
+
+  (maxFlow, residual)
+end edmondsKarp
